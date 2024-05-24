521cd13118aa2d77944fce9b4a4d2214b600dad6
Gyungmin Myung <gmmyung@kaist.ac.kr>
2024-05-25 00:31:27 +0900

    add friendly snippets to luasnip

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -30,7 +30,13 @@ return {
   {
     'L3MON4D3/LuaSnip',
     lazy = true,
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+    },
     build = 'make install_jsregexp',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
   },
 
   -- Autocompletion
