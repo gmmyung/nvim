8e35dec7ba85c480d5bfa02e316329bfec0952f7
Gyungmin Myung <gmmyung@kaist.ac.kr>
2024-06-15 23:44:02 +0900

    fix deprecated function

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,5 +1,6 @@
 return {
   'Exafunction/codeium.vim',
+  event = 'BufEnter',
   config = function()
     vim.g.codeium_disable_bindings = 1
     vim.keymap.set('i', '<C-CR>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
@@ -7,7 +8,7 @@ return {
       { expr = true, silent = true })
     vim.keymap.set('i', '<C-0>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
       { expr = true, silent = true })
-    vim.keymap.set('i', '<F5>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+    vim.keymap.set('i', '<F5>', function() return vim.fn['codeium#Complete']() end,
       { expr = true, silent = true })
   end
 }
