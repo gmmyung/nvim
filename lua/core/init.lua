0daf1221e2346e2a9a614c489d1026e185943688
Gyungmin Myung <gmmyung@kaist.ac.kr>
2025-06-08 00:06:13 +0900

    11.0

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -47,7 +47,14 @@ set_tab_settings('md', 2, true)
 set_tab_settings('json', 2, true)
 set_tab_settings('toml', 2, true)
 set_tab_settings('yaml', 2, true)
+
+-- XML
 set_tab_settings('html', 2, true)
+set_tab_settings('xml', 2, true)
+set_tab_settings('sdf', 2, true)
+set_tab_settings('svg', 2, true)
+set_tab_settings('urdf', 2, true)
+
 set_tab_settings('css', 2, true)
 set_tab_settings('js', 2, true)
 set_tab_settings('ts', 2, true)
@@ -105,3 +112,83 @@ vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter", "BufEnter" }, {
 		vim.cmd([[ startinsert ]])
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
+})
+
+vim.lsp.config('lua_ls', {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+					path ~= vim.fn.stdpath('config')
+					and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+			then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most
+				-- likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+				-- Tell the language server how to find Lua modules same way as Neovim
+				-- (see `:h lua-module-load`)
+				path = {
+					'lua/?.lua',
+					'lua/?/init.lua',
+				},
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+					-- Depending on the usage, you might want to add additional paths
+					-- here.
+					-- '${3rd}/luv/library'
+					-- '${3rd}/busted/library'
+				}
+				-- Or pull in all of 'runtimepath'.
+				-- NOTE: this is a lot slower and will cause issues when working on
+				-- your own configuration.
+				-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+				-- library = {
+				--   vim.api.nvim_get_runtime_file('', true),
+				-- }
+			}
+		})
+	end,
+	settings = {
+		Lua = {}
+	}
+})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config('cssls', {
+	capabilities = capabilities,
+})
+
+vim.lsp.config('html', {
+	capabilities = capabilities,
+})
+
+vim.lsp.config('jsonls', {
+	capabilities = capabilities,
+})
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('cmake')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('clangd')
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('cssls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('html')
---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,73 @@
+-- lua/plugins/cmp.lua
+
+return {
+	{
+		"neovim/nvim-lspconfig",
+		event = "VeryLazy",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body)
+					end,
+				},
+				window = {
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Cmdline setup
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" }
+				}
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" }
+				}, {
+					{ name = "cmdline" }
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false }
+			})
+
+			-- -- LSP capabilities setup
+			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- -- Replace <YOUR_LSP_SERVER> with your actual LSP server(s)
+			-- require("lspconfig")["lua_ls"].setup {
+			-- 	capabilities = capabilities
+			-- }
+		end,
+	},
+}
